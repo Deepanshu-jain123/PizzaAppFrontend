dcab3b820cbe7e469c19b06052fdbf569d6eb19a
Deepanshu <Deepanshu Jain>
2025-02-08 22:04:36 +0530

    comment some code

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   server: {
+//     port: 5172, // Default Vite port
+//     host: 'localhost', // Ensure it's set to 'localhost'
+//     open: true, // Opens browser on startup
+//   },
+// });
